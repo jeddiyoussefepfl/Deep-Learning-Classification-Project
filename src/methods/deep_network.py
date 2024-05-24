36dc9260700363d30c69426ea7e0eb6ea96baf03
jeddiyoussefepfl <150711367+jeddiyoussefepfl@users.noreply.github.com>
2024-05-24 16:25:46 +0200

    [WIP] Transformers

--- src/methods/deep_network.py
+++ src/methods/deep_network.py
@@ -106,13 +106,113 @@ class MyViT(nn.Module):
         Initialize the network.
         
         """
-        super().__init__()
+        super(MyViT, self).__init__()
         ##
         ###
         #### WRITE YOUR CODE HERE!
         ###
         ##
+        def get_positional_embeddings(sequence_length, d):
+            result = torch.ones(sequence_length, d)
+            for i in range(sequence_length):
+                for j in range(d):
+                    ### WRITE YOUR CODE HERE
+                    if j % 2 == 0:
+                        result[i,j] = np.sin(i / (10000 ** ((2 * j) / d)))
+                    else:
+                        result[i,j] = np.cos(i / (10000 ** ((2 * j) / d)))
 
+            return result
+        
+        class MyMSA(nn.Module):
+            def __init__(self, d, n_heads=2):
+                super(MyMSA, self).__init__()
+                self.d = d
+                self.n_heads = n_heads
+
+                assert d % n_heads == 0, f"Can't divide dimension {d} into {n_heads} heads"
+                d_head = int(d / n_heads)
+                self.d_head = d_head
+
+                self.q_mappings = nn.ModuleList([nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
+                self.k_mappings = nn.ModuleList([nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
+                self.v_mappings = nn.ModuleList([nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
+
+                self.softmax = nn.Softmax(dim=-1)
+
+            def forward(self, sequences):
+                result = []
+                for sequence in sequences:
+                    seq_result = []
+                    for head in range(self.n_heads):
+
+                        # Select the mapping associated to the given head.
+                        q_mapping = self.q_mappings[head]
+                        k_mapping = self.k_mappings[head]
+                        v_mapping = self.v_mappings[head]
+
+                        seq = sequence[:, head * self.d_head: (head + 1) * self.d_head]
+
+                        # Map seq to q, k, v.
+                        q, k, v = q_mapping(seq), k_mapping(seq), v_mapping(seq)
+
+                        m = nn.Softmax(dim=1)
+                        attention =  self.softmax((q @ k.T) / np.sqrt(self.d_head))
+                        seq_result.append(attention @ v)
+                    result.append(torch.hstack(seq_result))
+                return torch.cat([torch.unsqueeze(r, dim=0) for r in result])
+            
+            
+        class MyViTBlock(nn.Module):
+            def __init__(self, hidden_d, n_heads, mlp_ratio=4):
+                super(MyViTBlock, self).__init__()
+                self.hidden_d = hidden_d
+                self.n_heads = n_heads
+
+                self.norm1 = nn.LayerNorm(hidden_d) 
+                self.mhsa = MyMSA(hidden_d, n_heads)
+                self.norm2 = nn.LayerNorm(hidden_d)  
+                self.mlp = nn.Sequential( 
+                    nn.Linear(hidden_d, mlp_ratio * hidden_d),
+                    nn.GELU(),
+                    nn.Linear(mlp_ratio * hidden_d, hidden_d)
+                )
+
+            def forward(self, x):
+                out = x + self.mhsa(self.norm1(x))
+                out = out + self.mlp(self.norm2(out))
+                return out
+
+        self.chw = chw 
+        self.n_patches = n_patches
+        self.n_blocks = n_blocks
+        self.n_heads = n_heads
+        self.hidden_d = hidden_d
+
+        # Input and patches sizes
+        assert chw[1] % n_patches == 0 # Input shape must be divisible by number of patches
+        assert chw[2] % n_patches == 0
+        self.patch_size = (chw[1] / n_patches, chw[2] / n_patches) 
+
+        # Linear mapper
+        self.input_d = int(chw[0] * self.patch_size[0] * self.patch_size[1])
+        self.linear_mapper = nn.Linear(self.input_d, self.hidden_d)
+
+        # Learnable classification token
+        self.class_token = nn.Parameter(torch.rand(1, self.hidden_d))
+
+        # Positional embedding
+        self.positional_embeddings = get_positional_embeddings(n_patches ** 2 + 1, hidden_d) ### WRITE YOUR CODE HERE
+
+        # Transformer blocks
+        self.blocks = nn.ModuleList([MyViTBlock(hidden_d, n_heads) for _ in range(n_blocks)])
+
+        # Classification MLP
+        self.mlp = nn.Sequential(
+            nn.Linear(self.hidden_d, out_d),
+            nn.Softmax(dim=-1)
+        )
+        
     def forward(self, x):
         """
         Predict the class of a batch of samples with the model.
@@ -128,6 +228,48 @@ class MyViT(nn.Module):
         #### WRITE YOUR CODE HERE!
         ###
         ##
+        
+        def patchify(images, n_patches):
+            n, c, h, w = images.shape
+
+            assert h == w # Only square images are supported
+
+
+            patches = torch.zeros(n, n_patches ** 2, h * w * c // n_patches ** 2)
+            patch_size = h // n_patches
+
+            for i, image in enumerate(images):
+                for j in range(n_patches):
+                    for k in range(n_patches):
+                        temp = image[:, j * patch_size : (j+1) * patch_size,  k * patch_size : (k+1) * patch_size]
+                        patches[i, j * n_patches + k] = temp.flatten()
+
+            return patches
+        
+        n, c, h, w = x.shape
+
+        # Divide images into patches.
+        patches = patchify(x, self.n_patches)
+
+        # Map the vector corresponding to each patch to the hidden size dimension.
+        tokens = self.linear_mapper(patches)
+
+        # Add classification token to the tokens.
+        tokens = torch.cat((self.class_token.expand(n, 1, -1), tokens), dim=1)
+
+        # Add positional embedding.
+        preds = tokens + self.positional_embeddings.repeat(n,1,1)
+
+        # Transformer Blocks
+        for block in self.blocks:
+            preds = block(preds)
+
+        # Get the classification token only.
+        preds = preds[:, 0]
+
+        # Map to the output distribution.
+        preds = self.mlp(preds)
+        
         return preds
 
 
